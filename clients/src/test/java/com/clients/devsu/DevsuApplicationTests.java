94c5a6f2892c60ff952b45aa3d110a4d46424eb3
ASHMEDDF <ashmeddiaz5@gmail.com>
2024-06-25 18:49:37 -0500

    Code base for the two microservices

---
+++ clients/src/test/java/com/clients/devsu/DevsuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clients.devsu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevsuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
